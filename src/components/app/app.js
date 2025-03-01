d059358742bd78b5dec6e6d075471e70bbbeb326
Nikhil Yadav <ynikhilprakash@gmail.com>
2025-03-01 16:09:16 +0000

    add components

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>checking if it is still workig</p>
+    </div>
+  );
+}
+
+export default App;
